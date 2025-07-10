0de8678f9d0e7f542a8cef997a6a1844ea121697
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-07-10 21:55:02 +0600

    navbar design completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,12 +4,14 @@ import {navLists} from '../../utils/constant'
 const Navbar = () => {
     return (
         <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-            <nav className='flex screen-max-width w-full'>
+            <nav className='flex justify-between screen-max-width w-full'>
                 <img src={appleImg} alt="apple logo" />
-                {
-                    navLists.map((item) =><div className='flex max-sm:hidden' key={item}>{item}</div>)
-                }
-                <div>
+                <div className='flex max-sm:hidden'>
+                    {
+                    navLists.map((item) =><div className='text-sm text-gray-300 hover:text-white transition-all px-5 cursor-pointer' key={item}>{item}</div>)
+                    }
+                </div>
+                <div className='flex justify-baseline gap-7 max-sm:flex-1 max-sm:justify-end'>
                     <img src={searchImg} alt="search image" width={18} height={18} />
                     <img src={bagImg} alt="bag image" width={18} height={18} />
                 </div>
