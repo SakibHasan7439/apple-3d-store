70b617f33d82631e673a3e41bf8700d7959007df
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-07-13 22:51:54 +0600

    features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Features from "./Components/Features/Features"
 import Hero from "./Components/Hero/Hero"
 import Highlight from "./Components/Highlight/Highlight"
 import Models from "./Components/Models/Models"
 import Navbar from "./Components/Navbar/Navbar"
+import * as Sentry from "@sentry/react"
 
+// eslint-disable-next-line react-refresh/only-export-components
 function App() {
-
   return (
     <main>
         {/* navbar */}
@@ -15,8 +17,11 @@ function App() {
         <Highlight />
         {/* model */}
         <Models />
+        {/* features */}
+        <Features />
     </main>
   )
 }
 
-export default App
+// eslint-disable-next-line react-refresh/only-export-components
+export default Sentry.withProfiler(App)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://50bbf56b9b4535cb63d87fcb12799452@o4509656567644160.ingest.us.sentry.io/4509656572297216",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0,
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
